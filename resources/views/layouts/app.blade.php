62bd95c9bab3346e4effee9f17ac431da0e4765a
Jonas <jonahougo@gmail.com>
2021-08-24 09:20:14 +0300

    First commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,15 +12,15 @@
 <nav class="p-6 bg-white flex justify-between mb-6">
     <ul class="flex items-center">
         <li>
-            <a href="" class="p-3">Home</a>
+            <a href="/" class="p-3">Home</a>
         </li>
 
         <li>
-            <a href="" class="p-3">Dashboard</a>
+            <a href="{{route('dashboard')}}" class="p-3">Dashboard</a>
         </li>
 
         <li>
-            <a href="" class="p-3">Post</a>
+            <a href="{{route('posts')}}" class="p-3">Posts</a>
         </li>
     </ul>
 
@@ -29,11 +29,14 @@
         @auth
 
         <li>
-            <a href="" class="p-3">Alex Garett-Smith</a>
+            <a href="" class="p-3">{{ auth()->user()->name }}</a>
         </li>
 
         <li>
-            <a href="" class="p-3">Logout</a>
+            <form action="{{route('logout')}}" method="post" class="p-3 inline">
+                @csrf
+                <button type="submit">Logout</button>
+            </form>
         </li>
 
         @endauth
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,7 +18,13 @@ use App\Http\Controllers\Auth\RegisterController;
 |
 */
 
+Route::get('/', function(){
+    return view('home');
+})->name('home');
+
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'store']);
@@ -24,6 +32,5 @@ Route::post('/login', [LoginController::class, 'store']);
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
 
-Route::get('/posts', function () {
-    return view('posts.index');
-});
+Route::get('/posts', [PostController::class, 'index'])->name('posts');
+Route::post('/posts', [PostController::class, 'store']);
