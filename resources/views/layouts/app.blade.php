d2976b3a9e50a7e07f6ef46a7662aea0b53a42fd
Jona-Ougo <56861743+Jona-Ougo@users.noreply.github.com>
2021-08-24 10:15:17 +0300

    Update app.blade.php
    Dashboard removal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,9 +15,7 @@
             <a href="/" class="p-3">Home</a>
         </li>
 
-        <li>
-            <a href="{{route('dashboard')}}" class="p-3">Dashboard</a>
-        </li>
+        
 
         <li>
             <a href="{{route('posts')}}" class="p-3">Posts</a>
